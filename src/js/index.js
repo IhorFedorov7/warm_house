11f9dca591d94c98833bfca5d9310a91e7a99b2b
Ihor Fedorov <ihor.fedorov.life@gmail.com>
2022-01-28 01:47:51 +0200

    accordion add

--- src/js/index.js
+++ src/js/index.js
@@ -3,19 +3,21 @@ import objLang from './lang/lang.js';
 import createSlider from './components/slider/createSlider.js';
 import infoSlide from './components/slider/obj.js';
 import slider from './components/slider/slider.js';
+import accordion from './components/accordion/accordion.js';
 
-createSlider({
-    arr: infoSlide,
-    funSlider: () => {
-        slider({
-            act: [1,2,3]
-        })
-    }
+document.addEventListener('DOMContentLoaded', () => {
+    createSlider({
+        arr: infoSlide,
+        funSlider: () => {
+            slider({
+                act: [1,2,3]
+            })
+        }
+    });
+
+    accordion();
+    changeLang(objLang);
 });
-
-
-
-changeLang(objLang);
 
 
 
